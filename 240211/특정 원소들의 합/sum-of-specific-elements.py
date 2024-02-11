0e819301450322fbb25372ef499e0b71dd3a2f1b
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-11 17:11:54 +0900

    Title: 특정 원소들의 합, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240211/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,4 @@
+j = 0
+sum_val = 0
+arr = [list(map(int, input().split())) for _ in range(4)]
+print(arr[0][0] + arr[1][0] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2] + arr[3][0] + arr[3][1] + arr[3][2] + arr[3][3])
