2d5862592d25e1e05523502fceb8eab2bb8d3c3e
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-18 02:31:28 +0900

    Title: 문자에 따른 명령 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240218/문자에 따른 명령 2/text-based-commands2.py
+++ 240218/문자에 따른 명령 2/text-based-commands2.py
@@ -1,14 +1,16 @@
 x, y = 0, 0
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 dir_num = 3
-dist = 0
+
 for i in input():
-    
+    dist = 0
     if i == 'L':
         dir_num = (dir_num + 3) % 4
     elif i == 'R':
         dir_num = (dir_num + 1) % 4
     elif i == 'F':
         dist = 1
+    
     x, y = x + dx[dir_num] * dist, y + dy[dir_num] * dist
+    # print(f'x:{x}, y:{y}, dist: {dist}, dir_num: {dir_num}, i:{i}')
 print(x, y)
