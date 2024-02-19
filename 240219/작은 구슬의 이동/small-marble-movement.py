3e7d8f210914948bf86fe4ec8164bc465048af82
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-19 13:17:04 +0900

    Title: 작은 구슬의 이동, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240219/작은 구슬의 이동/small-marble-movement.py
+++ 240219/작은 구슬의 이동/small-marble-movement.py
@@ -2,7 +2,7 @@ n, t = map(int, input().split())
 x, y, d = input().split()
 x, y = int(x), int(y)
 dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
-mapper = {'U': 1, 'D': 2, 'R': 0, 'L': 3}
+mapper = {'U': 2, 'D': 1, 'R': 0, 'L': 3}
 direc = mapper[d]
 def in_range(x, y):
     return x > 0 and x <= n and y > 0 and y <= n
