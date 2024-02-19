d47ac7f787207155bd8c57ff697f5276c0af91e5
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-19 13:15:16 +0900

    Title: 작은 구슬의 이동, Time: 152ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240219/작은 구슬의 이동/small-marble-movement.py
+++ 240219/작은 구슬의 이동/small-marble-movement.py
@@ -5,15 +5,15 @@ dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
 mapper = {'U': 1, 'D': 2, 'R': 0, 'L': 3}
 direc = mapper[d]
 def in_range(x, y):
-    return x > 0 and x < n and y > 0 and y < n
+    return x > 0 and x <= n and y > 0 and y <= n
 
 for i in range(t):
     nx, ny = x + dxs[direc], y + dys[direc]
     if not in_range(nx, ny):
-        direc = 3 - mapper[d]
+        direc = 3 - direc
     else:
         x, y = x + dxs[direc], y + dys[direc]
-    # print(f'nx: {nx}, ny: {ny}, direc: {direc}')
-    # print(f'x: {x}, y: {y}, direc: {direc}')
+    # print(f'i: {i}, nx: {nx}, ny: {ny}, direc: {direc}')
+    # print(f'i: {i}, x: {x}, y: {y}, direc: {direc}')
 
 print(x, y)
