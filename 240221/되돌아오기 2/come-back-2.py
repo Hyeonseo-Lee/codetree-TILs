cea38f6310d850ec2bd2c6346eee99bee2975a2d
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-21 00:06:53 +0900

    Title: 되돌아오기 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/되돌아오기 2/come-back-2.py
+++ 240221/되돌아오기 2/come-back-2.py
@@ -10,10 +10,10 @@ ans = -1
 # 서 -> 남
 # 남 -> 동 
 # 동 -> 북
-# 북 동 서 남 
+# 북 동 남 서 
 dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 for i in sen:
-    # print(f'i: {i}, x: {x}, y: {y}, c_dir: {c_dir}, cnt: {cnt}')
+    #print(f'i: {i}, x: {x}, y: {y}, c_dir: {c_dir}, cnt: {cnt}')
     if i == 'F':
         x, y = x + dxs[c_dir], y + dys[c_dir]
         cnt += 1
@@ -21,7 +21,7 @@ for i in sen:
         c_dir = (c_dir + 1) % 4        
         cnt += 1
     elif i == 'L':
-        c_dir = (c_dir + 2) % 4      
+        c_dir = (c_dir + 3) % 4      
         cnt += 1
     if x == 0 and y == 0:
         ans = cnt
