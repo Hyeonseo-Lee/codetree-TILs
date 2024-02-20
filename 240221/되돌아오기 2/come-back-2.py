bdbe2ce5e07e3730892141cbd2afa87b78dbfe84
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-21 00:05:37 +0900

    Title: 되돌아오기 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/되돌아오기 2/come-back-2.py
@@ -0,0 +1,30 @@
+sen = input()
+x, y, cnt = 0, 0, 0
+c_dir = 0
+ans = -1
+# 북 -> 동
+# 동 -> 남
+# 남 -> 서
+# 서 -> 북
+# 북 -> 서
+# 서 -> 남
+# 남 -> 동 
+# 동 -> 북
+# 북 동 서 남 
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+for i in sen:
+    # print(f'i: {i}, x: {x}, y: {y}, c_dir: {c_dir}, cnt: {cnt}')
+    if i == 'F':
+        x, y = x + dxs[c_dir], y + dys[c_dir]
+        cnt += 1
+    elif i == 'R':
+        c_dir = (c_dir + 1) % 4        
+        cnt += 1
+    elif i == 'L':
+        c_dir = (c_dir + 2) % 4      
+        cnt += 1
+    if x == 0 and y == 0:
+        ans = cnt
+        break
+    
+print(ans)
