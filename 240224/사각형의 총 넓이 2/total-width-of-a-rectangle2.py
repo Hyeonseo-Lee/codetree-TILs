f4a253ba66a8494335b43ecd53171a8a53a6c56f
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-24 21:04:11 +0900

    Title: 사각형의 총 넓이 2, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240224/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240224/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,14 +1,15 @@
 n = int(input())
-arr = [[0] * 101 for _ in range(101)]
+arr = [[0] * 201 for _ in range(201)]
 
 cnt = 0
 for _ in range(n):
     x1, y1, x2, y2 = map(int, input().split())
+    x1, y1, x2, y2 = x1 + 100, y1 + 100, x2 + 100, y2 + 100
     for i in range(x1, x2):
         for j in range(y1, y2):
             arr[i][j] = 1
-for i in range(101):
-    for j in range(101):
+for i in range(201):
+    for j in range(201):
         if arr[i][j] == 1:
             cnt += 1
 print(cnt)
