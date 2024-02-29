6a8b213f200114a9fd9954fb85307d5cd5ae2bab
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-02-29 23:22:11 +0900

    Title: 만나는 그 순간, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240229/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+a_arr, b_arr = [0] * 1001, [0] * 1001
+
+def findtime(nm, arr):
+    start, place = 0, 0
+    for _ in range(nm):
+        d, t = input().split()
+        t = int(t)
+        if d == 'R':
+            for i in range(start+1, start + t + 1):
+                place += 1     
+                arr[i] = place   
+        else:
+            for i in range(start+1, start + t + 1):
+                place -= 1
+                arr[i] = place
+            
+        start = start + t
+        #print(f'd: {d}, t: {t}, start: {start}, place:{place}')
+   
+findtime(n, a_arr)
+findtime(m, b_arr)
+
+for i in range(1, 1001):
+    if a_arr[i] == b_arr[i]:
+        print(i)
+        break
