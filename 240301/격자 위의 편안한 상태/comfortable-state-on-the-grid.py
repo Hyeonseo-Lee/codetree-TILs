5cffab236dd421f94fb997e7cca55e853ddbc2e1
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-03-01 16:48:19 +0900

    Title: 격자 위의 편안한 상태, Time: 180ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240301/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240301/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 arr = [[0] * n for _ in range(n)]
 dxs, dys = [0, -1, 0, 1],[1, 0, -1, 0]
 def is_number(x, y):
-    return x >= 0 and x < n and y >= 0 and y < m
+    return x >= 0 and x < n and y >= 0 and y < n
 for _ in range(m):
     ans, cnt = 0, 0
     r, c = map(int, input().split())
@@ -10,7 +10,6 @@ for _ in range(m):
     for i, j in zip(dxs, dys):
         nx, ny = r + i - 1, c + j -1
         if is_number(nx, ny):
-
             if arr[r + i - 1][c + j - 1] == 1:
                 cnt += 1
             if cnt == 3:
