d5bedab972a3ea24371d270497d4d0fc01439b5d
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-03-01 16:47:27 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240301/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+arr = [[0] * n for _ in range(n)]
+dxs, dys = [0, -1, 0, 1],[1, 0, -1, 0]
+def is_number(x, y):
+    return x >= 0 and x < n and y >= 0 and y < m
+for _ in range(m):
+    ans, cnt = 0, 0
+    r, c = map(int, input().split())
+    arr[r - 1][c - 1] = 1
+    for i, j in zip(dxs, dys):
+        nx, ny = r + i - 1, c + j -1
+        if is_number(nx, ny):
+
+            if arr[r + i - 1][c + j - 1] == 1:
+                cnt += 1
+            if cnt == 3:
+                ans = 1
+                break
+    print(ans)
