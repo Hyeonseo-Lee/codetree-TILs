59ae12ca6275e9d605250eddca7a20c5ca5e15f0
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-06-23 00:51:46 +0900

    Title: 벌금은 누구에게, Time: 184ms, Memory: 25MB, Status: Passed - CodeTree

--- 240623/벌금은 누구에게/who-will-pay.py
+++ 240623/벌금은 누구에게/who-will-pay.py
@@ -1,9 +1,10 @@
 n, m, k = map(int, input().split())
 arr = [0]*n
-
+ans = -1
 for i in range(m):
     s = int(input())
     arr[s-1] += 1
     if arr[s-1] == k:
-        print(s)
+        ans = s
         break
+print(ans)
