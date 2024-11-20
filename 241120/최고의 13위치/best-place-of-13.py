738251a6182f183d5a1e0aafa3a6f67746f601f1
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-11-20 16:58:17 +0900

    Title: 최고의 13위치, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241120/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,12 @@
+import sys
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+minv = -sys.maxsize
+
+for i in range(n):
+    for j in range(n - 2):
+        num = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+    if num > minv:
+        minv = num
+print(minv)
+        
