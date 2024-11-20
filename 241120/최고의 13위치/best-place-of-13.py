6619a7266f51a5a9a4d467686859fc8e9066a5cc
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2024-11-20 17:02:32 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241120/최고의 13위치/best-place-of-13.py
+++ 241120/최고의 13위치/best-place-of-13.py
@@ -6,7 +6,8 @@ minv = -sys.maxsize
 for i in range(n):
     for j in range(n - 2):
         num = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
-    if num > minv:
-        minv = num
+        # print(num)
+        if num > minv:
+            minv = num
 print(minv)
         
