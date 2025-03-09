118e989e496581a376b05a7206dee8defccda52b
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2025-03-09 22:43:38 +0900

    Title: 체크판위에서 2, Time: 71ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250309/체크판위에서 2/on-the-checkboard-2.py
+++ 250309/체크판위에서 2/on-the-checkboard-2.py
@@ -5,12 +5,12 @@ answer = 0
 
 for i in range(a):
     for j in range(b):
-        for k in range(i+1, a):
-            for g in range(j+1, b):
+        for k in range(i+1, a-1):
+            for g in range(j+1, b-1):
                   
                 if grid[i][j] != grid[0][0] and grid[k][g] != grid[i][j] and grid[k][g] != grid[a-1][b-1]:
                     cnt += 1
-                    #print(i,j,grid[i][j],k,g,grid[k][g],h,s,grid[h][s],cnt)
+                    #print(i,j,grid[i][j],k,g,grid[k][g],cnt)
                             
 
 print(cnt)
